f7d0c52f9b4911df56e614c7657ffc189912c486
Kamil Lelonek <squixy.sln@gmail.com>
2015-05-20 20:30:29 +0200

    Fixed flux loading

--- assets/javascripts/main.jsx
+++ assets/javascripts/main.jsx
@@ -1,18 +1,21 @@
 import './vendor'
 import '../stylesheets/main.sass'
 
-import fetchData from './http_requests/fetch_data'
-import router    from './router'
-import Flux      from './flux/flux'
+import fetchData     from './http_requests/fetch_data'
+import router        from './router'
+import Flux          from './flux/flux'
+import FluxComponent from 'flummox/component'
 
 const flux = new Flux();
 
 router.run((Handler, state) =>
-    fetchData(state.routes)
-      .then(data => {
-        React.withContext(
-          { flux: flux },
-          () => React.render(<Handler data={ data } />, document.body)
-        )
-      })
+  fetchData(state.routes)
+    .then(data =>
+      React.render(
+        <FluxComponent flux={ flux }>
+          <Handler data={ data } { ...state } />
+        </FluxComponent>,
+        document.body
+      )
+    )
 );
